732e087e4e4c3ffa05b38891956eb4a4b3c942da
sulenchy <sulaimanabiodun172@gmail.com>
2019-09-15 11:42:25 +0100

    add webpack production setting

---
+++ webpack.common..js
@@ -0,0 +1,35 @@
+ const path = require('path');
+ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+ const HtmlWebPackPlugin = require('html-webpack-plugin');
+ const webpack = require("webpack");
+
+ module.exports = {
+   entry: './src/index.js',
+   module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+   plugins: [
+     new CleanWebpackPlugin(),
+     new webpack.HotModuleReplacementPlugin(),
+     new HtmlWebPackPlugin({
+        template: "./public/index.html",
+        filename: "./index.html"
+      })
+   ],
+   output: {
+     filename: 'bundle.js',
+     path: path.resolve(__dirname, 'dist/'),
+   }
+ };
